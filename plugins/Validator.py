6d7bf1b069f2a516cf4d34825075ef11305d38fe
Nard Janssens <nardjanssens@gmail.com>
2018-07-13 20:38:32 +0200

    Add files via upload

---
+++ plugins/Validator.py
@@ -0,0 +1,21 @@
+import os
+os.system("java -jar /home/nard/PhotonFileUtils-OpenGL/plugins/PhotonFileValidator/PhotonFileValidator.jar")
+
+"""
+import platform
+
+print ("test:")
+
+if platform.system()=="Linux":
+    # use 'which java' to find path
+    print ("Running Linux")
+
+if platform.system()=="Darwin": #Mac
+    None 
+
+if platform.system()=="Windows":
+    None 
+"""
+#Always have the return variable set!
+ret = "Succes"
+
