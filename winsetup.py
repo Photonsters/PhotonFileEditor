c95b638d6a223b7e00a8b1e772153ceb524f4ad2
X3msnake <X3msnake@users.noreply.github.com>
2018-07-26 16:57:07 +0100

    Added Open Gl - Now Working
    This script is already working and making a installer.
    
    There are 2 issues to resolve.
    1 - We need to find a way for the icon to get it's path automatically from the user selected install folder
    2 - The files are being installed locked and that is a problem with the settings.ini

--- winsetup.py
+++ winsetup.py
@@ -1,21 +1,33 @@
+# This script is working but needs a major overhaulling
+# Check https://github.com/makewitharduino/Scratio/blob/master/setup.py for inspiration
+# X3msnake 180726
+#
+################################
+#
+#just build	 			python winsetup.py build -b ..\							#Builds on the folder above
+#build installer		python winsetup.py build -b ..\  bdist_msi -d ..\		#Builds on the folder above
+#
+################################
+
+import os
 import sys
 from cx_Freeze import setup, Executable
 
 # https://stackoverflow.com/questions/15734703/use-cx-freeze-to-create-an-msi-that-adds-a-shortcut-to-the-desktop
 # http://msdn.microsoft.com/en-us/library/windows/desktop/aa371847(v=vs.85).aspx
 shortcut_table = [
-    ("DesktopShortcut",								# Shortcut
-     "DesktopFolder",								# Directory_
-     "Photon File Editor",							# Name
-     "TARGETDIR",									# Component_
-     "[TARGETDIR]PhotonEditor.exe",					# Target
-     None,											# Arguments
-     None,											# Description
-     None,											# Hotkey
-     "C:\Program Files (x86)\PhotonFileEditor\photonsters.ico",					# Icon
-     None,											# IconIndex
-     None,											# ShowCmd
-     'TARGETDIR'               						# WkDir
+    ("DesktopShortcut",												# Shortcut
+     "DesktopFolder",												# Directory_
+     "PhotonFileEditor",											# Name
+     "TARGETDIR",													# Component_
+     "[TARGETDIR]PhotonEditor.exe",									# Target
+     None,															# Arguments
+     None,															# Description
+     None,															# Hotkey
+     "C:\Program Files (x86)\PhotonFileEditor\photonsters.ico",		# Icon
+     None,															# IconIndex
+     None,															# ShowCmd
+     'TARGETDIR'               										# WkDir
      )
     ]
 
@@ -26,7 +38,11 @@ msi_data = {"Shortcut": shortcut_table}
 bdist_msi_options = {'data': msi_data}
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"],"excludes": ["tkinter"],"include_files": [""], "include_msvcr" : True}
+build_exe_options = {"packages": ["os", "OpenGL"],"include_files": [""], "include_msvcr" : True}
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -36,10 +52,8 @@ if sys.platform == "win32":
 
 setup(  name = "PhotonFileEditor",
         version = "0.1",
-        description = "Photon Files Editor",
+		author= "Photonsters",
+		url="https://github.com/Photonsters",
+        description = "Photon File Editor",
         options = {"build_exe": build_exe_options,"bdist_msi": bdist_msi_options},
         executables = [Executable("PhotonEditor.py", base=base,)])
-
-
-#just build	 	python setup.py build --build-base=../
-#installer		python setup.py build --build-base=../ bdist_msi
