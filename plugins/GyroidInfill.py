eb42e74120fdfb75673a75c8d6da8fa811d3656c
Nard Janssens <nardjanssens@gmail.com>
2018-08-28 20:01:45 +0200

    plugin for BinaryVoxels/gyroid.js

---
+++ plugins/GyroidInfill.py
@@ -0,0 +1,25 @@
+import os
+# global filename contains full path of opened PhotonFile in PhotonFileEditor
+
+os.system("node BinaryVoxels/gyroid.js -s "+filename)
+
+"""
+# Some extra code, maybe needed in future releases
+import platform
+
+print ("test:")
+
+if platform.system()=="Linux":
+    # use 'which java' to find path
+    print ("Running Linux")
+
+if platform.system()=="Darwin": #Mac
+    None 
+
+if platform.system()=="Windows":
+    None 
+"""
+
+#Always have the return variable set!
+ret = "Succes"
+
